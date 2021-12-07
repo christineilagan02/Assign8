58f9177c24dd8bb52d2e41bb1b0c73e1636b8a41
unknown <christineilagan02@gmail.com>
2021-12-07 12:25:26 +0800

    Prog 1: Lottery coding the menu

--- prog1.py
+++ prog1.py
@@ -7,3 +7,49 @@
 # If the user enter “y” the user will play again
 # if “n” the program will exit.
 
+import random
+import sys
+
+NUMBER_OF_PICKS = 3
+MIN_PICK = 0
+MAX_PICK = 9
+
+def intro():
+    print("Welcome to Tine's Lottery")
+    name = input("Kindly, enter your name: \n>> ")
+    print(f"Hello {name.title()}! ") 
+    print("Are you ready to play?")
+
+def _try():
+    answer = input("Kindly, type y for yes and n for no. \n>> ")
+    if answer.lower() == "y":
+        return answer
+    elif answer.lower() == "n":
+        print("You can now exit.")
+        sys.exit()
+    else: 
+        print("Sorry your input must be a y/n!")
+        return _try()
+
+def title():
+    title = "\t      \33[33m**\33[0m \33[1mTine's Lottery\33[0m \33[33m**\33[0m"
+    print("\n\t\33[1m\33[33m*\33[0m---+---+---+---\33[1m\33[33m*\33[0m---+---+---+---\33[1m\33[33m*\33[0m")
+    print(title)
+    print("\t\33[1m\33[33m*\33[0m---+---+---+---\33[1m\33[33m*\33[0m---+---+---+---\33[1m\33[33m*\33[0m")
+
+def lottery_menu():
+     menu_list = ["\t           \33[3m\33[1m1. Play\33[0m", "\t           \33[3m\33[1m2. Exit\33[0m"]
+     print(menu_list[0])
+     print(menu_list[1])
+
+    
+
+
+
+def main():
+    intro()
+    _try()
+    title()
+    lottery_menu()
+
+main()
