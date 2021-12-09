4fe1908af2291bacb2314d91598f662eb17b9a9b
unknown <christineilagan02@gmail.com>
2021-12-09 22:32:42 +0800

    Prog 2: Guess the number code is in the def function ready

--- prog2.py
+++ prog2.py
@@ -5,13 +5,38 @@
 # Display “Less than” if the inputed number is less than the random number
 # Repeat asking the user until the random number has been guessed correctly.
 
+import sys
+
 def intro():
     print("\n\tWelcome to Tine's Guessing Number")
     name = input("\n\tHello! What is your name? \n\t>> ")
     print(f"Well {name.title()} before we begin, let's learn how to play the game.")
-    print("The computer will ask you for a number, and you will have to guess what it will give you.")
+    instruction()
+    print("Are you ready to guess a number?")
+
+def instruction():
+    from time import sleep
+    string = ("\n\t\33[3mThe computer will ask you for a number,\nand you will have to guess what it will give you.\33[0m\n")
+    for letter in string:
+        sleep(0.15)
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+
+def ready():
+    answer = input("\t\nKindly, type y for yes and n for no. \n\t    >> ")
+    print("\n")
+    if answer.lower() == "y":
+        return answer
+    elif answer.lower() == "n":
+        print("\t Ok come back when you're ready! ")
+        print("\t You can now exit.")
+        sys.exit("\n")
+    else: 
+        print(" Sorry your input must be a y/n!")
+        return ready()
 
 def main():
     intro()
+    ready()
 
 main()
